4a772e25d47c0b55e74886426b75512a0562b5c7
vinmor12 <77013609+vinmor12@users.noreply.github.com>
2023-09-12 22:59:30 +0200

    Update text_detection.py

--- notebooks/004-hello-detection/text_detection.py
+++ notebooks/004-hello-detection/text_detection.py
@@ -12,25 +12,23 @@ from openvino.runtime import Core
 print("Modules Have Been Imported ")
 
 # LOAD MODEL ------------------------------------------------------------------
-print("...")
-print("Write the Path of Model ... ")
-print("(example: model/horizontal-text-detection-0001.xml)")
+print("\nWrite the Path of Model:\n(example: model/horizontal-text-detection-0001.xml)")
 model_path = input()
 # Initialize
 ie = Core()
 # Read OpenVINO IR model
 model = ie.read_model(model=model_path)
 # Compile OpenVINO IR model
-compiled_model = ie.compile_model(model=model, device_name="CPU")
+print("\nWrite the target Device:\n(example: CPU)")
+target_device = input()
+compiled_model = ie.compile_model(model=model, device_name=target_device)
 # Store input and output layers
 input_layer_ir = compiled_model.input(0)
 output_layer_ir = compiled_model.output("boxes")
 
 # LOAD IMAGE ------------------------------------------------------------------
 # Text detection models expect an image in BGR format.
-print("...")
-print("Write the Path of Input Image ... ")
-print("(example: ../data/image/intel_rnb.jpg)")
+print("\nWrite the Path of Input Image:\n(example: ../data/image/intel_rnb.jpg)")
 image_path = input()
 image = cv2.imread(image_path)
 # Define batch size, number of channels, height, width.
@@ -41,8 +39,7 @@ resized_image = cv2.resize(image, (W, H))
 input_image = np.expand_dims(resized_image.transpose(2, 0, 1), 0)
 # Plot image RGB
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB));
-print("...")
-print("Close the Image to Continue ...")
+print("\nClose the Image to Continue ...")
 plt.show()
 
 # INFERENCE -------------------------------------------------------------------
@@ -51,8 +48,7 @@ boxes = compiled_model([input_image])[output_layer_ir]
 # Remove zero only boxes.
 boxes = boxes[~np.all(boxes == 0, axis=1)]
 # Print boxes info
-print("Boxes: ")
-print(boxes)
+print("\nBoxes:\n",boxes)
 
 # RESULT ----------------------------------------------------------------------
 # For each detection, the description is in the [x_min, y_min, x_max, y_max, conf] format:
@@ -79,10 +75,12 @@ def convert_result_to_image(bgr_image, resized_image, boxes, threshold, conf_lab
                 else int(corner_position * ratio_x)
                 for idx, corner_position in enumerate(box[:-1])
             ]
-            # Draw a box based on the position, parameters in rectangle function are: image, start_point, end_point, color, thickness.
+            # Draw a box based on the position, parameters in rectangle function are: 
+            # image, start_point, end_point, color, thickness.
             rgb_image = cv2.rectangle(rgb_image, (x_min, y_min), (x_max, y_max), colors["green"], 3)
             # Add text to the image based on position and confidence.
-            # Parameters in text function are: image, text, bottom-left_corner_textfield, font, font_scale, color, thickness, line_type.
+            # Parameters in text function are: image, text, bottom-left_corner_textfield, 
+            # font, font_scale, color, thickness, line_type.
             if conf_labels:
                 rgb_image = cv2.putText(
                     rgb_image,
@@ -97,13 +95,12 @@ def convert_result_to_image(bgr_image, resized_image, boxes, threshold, conf_lab
     return rgb_image
 # Plot result
 # Write threshold
-print("Write threshold: ")
-print("example: 0.3")
+print("\nWrite threshold:\nexample: 0.3")
 threshold = float(input())
 plt.figure(figsize=(10, 6))
 plt.axis("off")
 plt.imshow(convert_result_to_image(image, resized_image, boxes, threshold, conf_labels=True));
-print("Result ...")
+print("\nResult ...")
 plt.show()
 
 # Vincenzo
